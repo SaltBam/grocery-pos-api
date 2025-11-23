2e11617fbf7ddfd78290e8df85a0a2761a462aff
SaltinStillWaters <SaltinStillWaters@gmail.com>
2025-11-23 12:40:57 +0800

    feat: implemented inventory get all

--- src/inventory-man/inventory/inventory.controller.ts
+++ src/inventory-man/inventory/inventory.controller.ts
@@ -1,4 +1,17 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
+import { InventoryService } from './inventory.service';
+import { BaseResponse } from 'src/common/base/base.response';
 
 @Controller('inventory')
-export class InventoryController {}
+export class InventoryController {
+    constructor(
+        private service: InventoryService,
+    ) {}
+
+    @Get('all')
+    async getAll() {
+        const data = await this.service.getAll();
+
+        return new BaseResponse(data);
+    }
+}
--- src/inventory-man/inventory/inventory.module.ts
+++ src/inventory-man/inventory/inventory.module.ts
@@ -1,8 +1,16 @@
 import { Module } from '@nestjs/common';
 import { InventoryService } from './inventory.service';
 import { InventoryController } from './inventory.controller';
+import { MongooseModule } from '@nestjs/mongoose';
+import { Inventory, InventorySchema } from './inventory.schema';
 
 @Module({
+  imports: [
+    MongooseModule.forFeature([{
+      name: Inventory.name,
+      schema: InventorySchema
+    }])
+  ],
   providers: [InventoryService],
   controllers: [InventoryController]
 })
